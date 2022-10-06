d606562faff1784eac35923db907edaa043a7f47
Okalis <115192289+okalis-snowbadge2@users.noreply.github.com>
2022-10-07 01:24:31 +0200

    Update streamlit_app.py
    added breaktast menu header and 3 Items

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,10 @@ streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
+streamlit.title('My Mom's New Healthy Diner')
+
+streamlit.header(& & 'Breakfast Favorites')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('© Kale, Spinach & Rocket Smoothie')
+streamlit.text('4) Hard-Boiled Free-Range Egg')
+streamlit. text("O Avocado Toast")
