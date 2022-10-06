f227f3a262a60f8426963b6717bda9771bb90ab7
Okalis <115192289+okalis-snowbadge2@users.noreply.github.com>
2022-10-07 01:26:25 +0200

    Update streamlit_app.py
    Fixe SyntaxError: invalid syntax with ('')

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
-streamlit.title('My Mom's New Healthy Diner')
+streamlit.title('My Mom''s New Healthy Diner')
 
 streamlit.header(& & 'Breakfast Favorites')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
