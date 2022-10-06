8e063b53a2f5594786202e79d6b79b902f36da33
Okalis <115192289+okalis-snowbadge2@users.noreply.github.com>
2022-10-07 01:41:33 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,5 +11,5 @@ streamlit. text("O Avocado Toast")
 
 
 import pandas
-my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruitmacros.txt")
-streamlit.dataframe(my fruit list)
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
