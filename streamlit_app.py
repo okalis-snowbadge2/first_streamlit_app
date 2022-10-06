3ac2ce9acbb8cb98ffec0d7c612ebb2a3fd056e6
Okalis <115192289+okalis-snowbadge2@users.noreply.github.com>
2022-10-07 01:27:39 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,8 +9,8 @@ streamlit.text('Hard-Boiled Free-Range Egg')
 
 streamlit.title('My Mom''s New Healthy Diner')
 
-streamlit.header(& & 'Breakfast Favorites')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.header('Breakfast Favorites')
+streamlit.text('& Omega 3 & Blueberry Oatmeal')
 streamlit.text('© Kale, Spinach & Rocket Smoothie')
 streamlit.text('4) Hard-Boiled Free-Range Egg')
 streamlit. text("O Avocado Toast")
