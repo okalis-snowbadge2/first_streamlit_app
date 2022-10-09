894bf4b2314d69406a77878d99c0ea992f4e3196
Okalis <115192289+okalis-snowbadge2@users.noreply.github.com>
2022-10-09 14:06:17 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit. text('Avocado Toast')
 
 
-streamlit.header('🍌' 'Build Your Own Fruit Smoothie')
+streamlit.header('🍌' ' ' 'Build Your Own Fruit Smoothie')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
