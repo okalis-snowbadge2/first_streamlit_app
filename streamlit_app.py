d4570ec7f7bc801b8badfbbe3910fce12d06a669
Okalis <115192289+okalis-snowbadge2@users.noreply.github.com>
2022-10-09 14:18:38 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,13 +3,13 @@ import streamlit
 streamlit.title('My Mom''s New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
-streamlit. text('Avocado Toast')
+streamlit.text('🍜' ' ' 'Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥬' ' ' 'Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔' ' ' 'Hard-Boiled Free-Range Egg')
+streamlit. text('🥑' ' ' 'Avocado Toast')
 
 
-streamlit.header('🍌' ' ' 'Build Your Own Fruit Smoothie')
+streamlit.header('🍌 🍊' ' ' 'Build Your Own Fruit Smoothie' ' ' '🥝 🍇')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
